7d7b1aee560d6f391813cd621f7795bc807e97b5
iimiji <mijik@sfu.ca>
2022-11-18 15:59:42 -0800

    Nov 18, update for submission

--- js/script.js
+++ js/script.js
@@ -17,12 +17,26 @@ window.onscroll = () => {
 	navlist.classList.remove('open');
 };
 
-const sr = ScrollReveal ({
+/*const sr = ScrollReveal ({
 	distance: '30px',
 	duration: 2600,
 	reset: true
-})
+});*/
 
-sr.reveal('.home-text',{delay:280, origin:'bottom'})
 
-sr.reveal('.featured,.cta,.new,.brand,.contact',{delay:200, origin:'bottom'})
+//display none /create class
+
+const ratingsEl = document.querySelectorAll(".rating");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
+
+ratingsEl.forEach((el) => {
+  el.addEventListener("click", () => {
+    ratingsEl.forEach((innerEl) => {
+      innerEl.classList.remove("active");
+    });
+
+    el.classList.add("active");
+  });
+});
+
